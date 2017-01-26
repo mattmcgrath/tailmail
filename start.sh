565fb37aacc3d17c56e7cb622857b29f9bd1cf40
Matthew R. McGrath <matthew.r.mcgrath@gmail.com>
2017-01-26 17:37:06 +0000

    Initial commit.

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ruby /home/pi/tailmail/tailmail.rb /home/pi/tailmail/watchlist.csv >> /home/pi/tailmail/tailmail.log &
---
+++ tailmail.rb
@@ -0,0 +1,38 @@
+require 'socket'
+require 'csv'
+require 'rushover'
+require 'json'
+
+settings = JSON.parse(File.read('config.json'))
+
+port = 30003 # dump1090 offers CSV data on this port
+
+socket = TCPSocket.new('localhost', port)
+client = Rushover::Client.new(settings['appkey'])
+user_key = settings['apikey']
+
+
+
+aircraft = {}
+watchlist = {}
+
+puts "Opening watchlist file #{ARGV[0]}..."
+CSV.foreach(ARGV[0]) do |row|
+  watchlist[row[0]] = {tailnumber: row[1], model: row[2], owner: row[3]}
+end
+
+puts "Listening on port #{port}..."
+while line = socket.gets
+  icao = line.split(',')[4]
+  if watchlist.key?(icao)
+    d = watchlist[icao]
+    time = Time.now
+    if aircraft[icao].nil?
+      aircraft[icao] = time
+      puts "Saw #{d[:tailnumber]} (#{d[:owner]}) at #{time}."
+      client.notify(user_key, "Saw #{d[:tailnumber]} (#{d[:owner]}) at #{time}.", priority: 1, title: 'Aircraft Notification')
+    end
+  end
+end
+
+socket.close
