941e2224f17f88b5e1fa461ad1a4a4675af82d02
Matthew R. McGrath <matthew.r.mcgrath@gmail.com>
2017-06-17 16:03:32 +0000

    Updated timing to only alert once per hour.

--- tailmail.rb
+++ tailmail.rb
@@ -11,14 +11,11 @@ socket = TCPSocket.new('localhost', port)
 client = Rushover::Client.new(settings['appkey'])
 user_key = settings['apikey']
 
-
-
-aircraft = {}
 watchlist = {}
 
 puts "Opening watchlist file #{ARGV[0]}..."
 CSV.foreach(ARGV[0]) do |row|
-  watchlist[row[0]] = {tailnumber: row[1], model: row[2], owner: row[3]}
+  watchlist[row[0]] = {tailnumber: row[1], model: row[2], owner: row[3], last_heard: 0}
 end
 
 puts "Listening on port #{port}..."
@@ -26,11 +23,11 @@ while line = socket.gets
   icao = line.split(',')[4]
   if watchlist.key?(icao)
     d = watchlist[icao]
-    time = Time.now
-    if aircraft[icao].nil?
-      aircraft[icao] = time
-      puts "Saw #{d[:tailnumber]} - #{d[:model]} (#{d[:owner]}) at #{time}."
-      client.notify(user_key, "Saw #{d[:tailnumber]} - #{d[:model]} (#{d[:owner]}) at #{time}.", priority: 1, title: 'Aircraft Notification')
+    if d[:last_heard] < Time.now.to_i - 3600 # If we haven't heard this aircraft before or within the last hour, send an alert
+      d[:last_heard] = Time.now.to_i
+      watchlist[icao] = d
+      puts "Saw #{d[:tailnumber]} - #{d[:model]} (#{d[:owner]}) at #{Time.at(d[:last_heard])}."
+      client.notify(user_key, "Saw #{d[:tailnumber]} - #{d[:model]} (#{d[:owner]}) at #{Time.at(d[:last_heard])}.", priority: 1, title: 'Aircraft Notification')
     end
   end
 end
